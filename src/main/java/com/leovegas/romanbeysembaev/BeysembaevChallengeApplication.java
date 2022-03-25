b502b76049fe5a441d02cb1fccbba13e9b25a924
Roman Beysembaev <r.beysembaev94@gmail.com>
2022-03-25 22:26:25 +0300

    init

---
+++ src/main/java/com/leovegas/romanbeysembaev/BeysembaevChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.leovegas.romanbeysembaev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeysembaevChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeysembaevChallengeApplication.class, args);
+	}
+
+}
